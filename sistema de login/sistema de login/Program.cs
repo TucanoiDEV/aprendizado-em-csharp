7c781655ca935f08c7875ee8468492367b2fe964
Rodrigo Henrique <150493553+TucanoiDEV@users.noreply.github.com>
2026-04-04 19:20:02 -0300

    conclusão sistema de login

--- sistema de login/sistema de login/Program.cs
+++ sistema de login/sistema de login/Program.cs
@@ -4,93 +4,117 @@ class Program
 {
     static void Main()
     {
-        string nome;
-        string senha;
-        string opcaoEscolhida;
-        bool finalizarCodigo = false;
+        string opcaoEscolhida = string.Empty;
 
         List<string> usuariosNome = new List<string>();
         List<string> usuariosSenha = new List<string>();
 
-        while (finalizarCodigo == false)
+        while (true)
         {
-            Console.Clear();
-            Console.WriteLine("==========\nBem vindo ao sistema de login!!!\n==========".ToUpper());
-            Console.WriteLine("\n\nO que deseja fazer?\n1 - Cadastrar\n2 - Logar\n3 - Conferir lista de usuários\n4 - Sair");
-            Console.Write("\nDigite o número da opção escolhida: ");
-            opcaoEscolhida = Console.ReadLine();
-            if (opcaoEscolhida == "1")
+            Menu(ref opcaoEscolhida);
+            
+            switch(opcaoEscolhida)
             {
-                Console.Clear();
+                case "1":
+                    Cadastrar(usuariosNome, usuariosSenha);
+                    break;
 
-                Console.Write("Digite seu nome de usuário: ");
-                nome = Console.ReadLine();
-                Console.Write("Digite sua senha: ");
-                senha = Console.ReadLine();
-                Console.Clear();
+                case "2":
+                    Logar(usuariosNome, usuariosSenha);
+                    break;
 
-                Console.WriteLine("\nCadastro realizado com sucesso!!!");
-                Console.ReadKey();
+                case "3":
+                    ListarUsuarios(usuariosNome);
+                    break;
 
-                usuariosNome.Add(nome);
-                usuariosSenha.Add(senha);
+                case "4":
+                    Sair();
+                    break;
 
-                continue;
-            }
-            else if(opcaoEscolhida == "2")
-            {
-                Console.Clear();
-
-                Console.Write("Digite seu nome de usuário: ");
-                nome = Console.ReadLine();
-                Console.Write("Digite sua senha: ");
-                senha = Console.ReadLine();
-
-                Console.Clear();
-
-                // Verificar se o nome de usuário existe e se a senha corresponde a esse nome de usuário a partir de suas posições nas listas
-                int indexUsuario = usuariosNome.IndexOf(nome);
-
-                if(indexUsuario != -1 && usuariosNome.IndexOf(nome) == usuariosSenha.IndexOf(senha))
-                {
-                    Console.WriteLine($"Login realizado com sucesso!!!\nBem vindo {nome}!!!");
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida, tente novamente!!!");
                     Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
                     Console.ReadKey();
-                }
-                else
-                {
-                    Console.WriteLine("Usuário ou senha incorretos, tente novamente!!!");
-                    Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
-                    Console.ReadKey();
+
                     continue;
-                }
-
-            }
-            else if (opcaoEscolhida == "3")
-            {
-                Console.Clear();
-                foreach (string usuario in usuariosNome)
-                {
-                    Console.WriteLine(usuario);
-                }
-                Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
-                Console.ReadKey();
-            }
-            else if(opcaoEscolhida == "4")
-            {
-                Console.Clear();
-                Console.WriteLine("Obrigado por usar o sistema de login!!!");
-                finalizarCodigo = true;
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Opção inválida, tente novamente!!!");
-                Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
-                Console.ReadKey();
-
-                continue;
             }
         }
+    }
+
+    static string Menu(ref string opcaoEscolhida)
+    {
+        Console.Clear();
+        Console.WriteLine("==========\nBem vindo ao sistema de login!!!\n==========".ToUpper());
+        Console.WriteLine("\n\nO que deseja fazer?\n1 - Cadastrar\n2 - Logar\n3 - Conferir lista de usuários\n4 - Sair");
+        Console.Write("\nDigite o número da opção escolhida: ");
+        opcaoEscolhida = Console.ReadLine();
+        return opcaoEscolhida;
+    }
+
+    static void Cadastrar(List<string> usuariosNome, List<string> usuariosSenha)
+    {
+        string nome = string.Empty;
+        string senha = string.Empty;
+
+        Console.Clear();
+
+        Console.Write("Digite seu nome de usuário: ");
+        nome = Console.ReadLine();
+        Console.Write("Digite sua senha: ");
+        senha = Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("Cadastro realizado com sucesso!!!");
+        Console.ReadKey();
+
+        usuariosNome.Add(nome);
+        usuariosSenha.Add(senha);
+    }
+
+    static void Logar(List<string> usuariosNome, List<string> usuariosSenha)
+    {
+        string nome = string.Empty;
+        string senha = string.Empty;
+
+        Console.Clear();
+
+        Console.Write("Digite seu nome de usuário: ");
+        nome = Console.ReadLine();
+        Console.Write("Digite sua senha: ");
+        senha = Console.ReadLine();
+
+        Console.Clear();
+
+        int posicaoNome = usuariosNome.IndexOf(nome);
+
+        if (posicaoNome != -1 && usuariosSenha[posicaoNome] == senha)
+        {
+            Console.WriteLine($"Login realizado com sucesso!!!\nBem vindo {nome}!!!");
+            Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("Usuário ou senha incorretos, tente novamente!!!");
+            Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey();
+        }
+    }
+    static void ListarUsuarios(List<string> usuariosNome)
+    {
+        Console.Clear();
+        foreach (string usuario in usuariosNome)
+        {
+            Console.WriteLine(usuario);
+        }
+        Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+        Console.ReadKey();
+    }
+    static void Sair()
+    {
+        Console.Clear();
+        Console.WriteLine("Saindo do programa...");
+        Environment.Exit(0);
     }
 }
